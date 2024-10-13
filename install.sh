6042860f8faee3b3e159415144791597030693bc
Erfan <erfjab@gmail.com>
2024-10-14 02:45:45 +0330

    bump to v0.2.3

--- install.sh
+++ install.sh
@@ -110,7 +110,7 @@ check_needs() {
 menu() {
     while true; do
         print "\n\t Welcome to Backuper!"
-        print "\t\t version 0.2.1 by @ErfJab"
+        print "\t\t version 0.2.3 by @ErfJab"
         print "—————————————————————————————————————————————————————————————————————————"
         print "1) Install"
         print "2) Manage"
